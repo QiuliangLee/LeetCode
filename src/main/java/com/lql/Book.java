7e8a963175c32e77be41219a6016e1ce907cab21
shushu <liqiuliang@bilibili.com>
2023-07-16 13:03:37 +0800

    little chagne
    Signed-off-by: shushu <liqiuliang@bilibili.com>

---
+++ src/main/java/com/lql/Book.java
@@ -0,0 +1,25 @@
+package com.lql;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    //id
+    private Long id;
+    //书名
+    private String name;
+
+    //分类
+    private String category;
+
+    //评分
+    private Integer score;
+
+    //简介
+    private String intro;
+
+}
